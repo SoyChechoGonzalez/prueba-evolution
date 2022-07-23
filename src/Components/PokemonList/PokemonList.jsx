61ef19160bfa29ce2fd2b3eda87e0140aa7a04a8
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2022-07-23 14:21:21 -0500

    Se corrige nombre de la función renderHP por renderStat

--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -11,7 +11,7 @@ const PokemonList = ({pokemons}) => {
 			     alt={props.record.name} title={props.record.name} />
 		);
 	};
-	const renderHP = (props) => {
+	const renderStat = (props) => {
 		console.log(props.value);
 		return (
 			<span key={props.value}>{props.value}</span>
@@ -33,42 +33,42 @@ const PokemonList = ({pokemons}) => {
 			displayName: 'Hp',
 			inputFilterable: true,
 			sortable: true,
-			render: renderHP
+			render: renderStat
 		},
 		{
 			name: `stats.${1}.base_stat`,
 			displayName: 'Ataque',
 			inputFilterable: true,
 			sortable: true,
-			render: renderHP
+			render: renderStat
 		},
 		{
 			name: `stats.${2}.base_stat`,
 			displayName: 'Defensa',
 			inputFilterable: true,
 			sortable: true,
-			render: renderHP
+			render: renderStat
 		},
 		{
 			name: `stats.${3}.base_stat`,
 			displayName: 'Ataque Especial',
 			inputFilterable: true,
 			sortable: true,
-			render: renderHP
+			render: renderStat
 		},
 		{
 			name: `stats.${4}.base_stat`,
 			displayName: 'Defensa Especial',
 			inputFilterable: true,
 			sortable: true,
-			render: renderHP
+			render: renderStat
 		},
 		{
 			name: `stats.${5}.base_stat`,
 			displayName: 'Velocidad',
 			inputFilterable: true,
 			sortable: true,
-			render: renderHP
+			render: renderStat
 		}
 	];
 	
